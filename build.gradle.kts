7f0456a7d4c2749dc2ba8cbf9314733f197e61f3
Till Krullmann <till.krullmann@gmx.net>
2019-06-13 18:32:08 +0200

    Explicitly specify Java version compatibility as 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,12 @@ subprojects {
     }
 
     plugins.withId("java") {
+
+        configure<JavaPluginConvention> {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+        
         dependencies {
             "compileOnly"("com.google.code.findbugs:jsr305")
             "testImplementation"("org.junit.jupiter:junit-jupiter-api")
